deb4ee74a951e04bae07a9e310d51b1b7c09b256
SkaptoryX <roberto.escobarn@usm.cl>
2024-11-21 01:50:45 -0300

    vistas ortogonales arregladas

--- MERN/frontend/src/components/ImagenSagital.jsx
+++ MERN/frontend/src/components/ImagenSagital.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+const ImagenSagital = ({ imageId }) => {
+  const imageRef = useRef(null);
 
-export default function Imagen(props) {
+  useEffect(() => {
+      if (!imageId || !imageRef.current) return;
+
+      const loadAndViewImage = async () => {
+          try {
+              if (!cornerstone.getEnabledElement(imageRef.current)) {
+                  cornerstone.enable(imageRef.current);
+              }
+
+              const image = await cornerstone.loadAndCacheImage(imageId);
+              cornerstone.displayImage(imageRef.current, image);
+          } catch (error) {
+              console.error('Error al cargar/mostrar la imagen sagital:', error);
+          }
+      };
+
+      loadAndViewImage();
+
+      return () => {
+          if (imageRef.current && cornerstone.getEnabledElement(imageRef.current)) {
+              cornerstone.disable(imageRef.current);
+          }
+      };
+  }, [imageId]);
+
   return (
-    <div>
-        <img src={`./public/vistas/sagital/sagital_${props.slide}.png`} style={{ width: '15%', height: 'auto' }}/>
-    </div>
-  )
-}
+      <div 
+          ref={imageRef}
+          style={{ 
+              width: '512px', 
+              height: '512px',
+              border: '1px solid black',
+              margin: '10px auto'
+          }}
+      />
+  );
+};
 
-
-`as;ldkasl;dk;asdkasl;da`
-
-
+export default ImagenSagital;
